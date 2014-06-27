46f3d037a8db68b1539180e36b598160cf6913e0
Kyle Lahnakoski <klahnakoski@mozilla.com>
2014-06-26 23:01:49 -0400

    whitespace and remove docs

--- tests/lib/A.js
+++ tests/lib/A.js
@@ -6,13 +6,12 @@
 // Author: Kyle Lahnakoski  (kyle@lahnakoski.com)
 ////////////////////////////////////////////////////////////////////////////////
 
-
 importScript("B.js");   //DEPENDS ON B and C, ANY ORDER IS FINE
 importScript("C.js");
 
 var A=function(){
 	$("#message").append("<div>A.js has been loaded</div>");
-	
+
 	B();
 	C();
 };
